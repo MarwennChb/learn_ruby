0de2efea3a25af62f5d75fef01d52ab06bd22847
marwenchb <marwenchb@gmail.com>
2018-01-16 14:06:52 +0100

    Code Calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,25 @@
 #write your code here
+
+def add (num1, num2)
+	num1 + num2
+end
+
+def subtract (num1, num2)
+	num1 - num2
+end
+
+def sum (arr)
+	res = 0
+	arr.each do |x|
+		res += x
+	end
+	return res
+end
+
+def multiply (arr)
+	multi = 0
+	arr.each do |x|
+		multi *= x
+	end
+	return multi 
+end
