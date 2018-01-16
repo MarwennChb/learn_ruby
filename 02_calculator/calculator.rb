76dceb2bfdfac282b45bffa5c6139b6200265947
marwenchb <marwenchb@gmail.com>
2018-01-16 18:39:28 +0100

    Mise à jour de calculator avec els commentaires sur le code

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -9,8 +9,8 @@ def subtract (num1, num2)
 end
 
 def sum (arr)
-	res = 0
-	arr.each do |x|
+	res = 0 #on initialise la somme "res" (pour résultat) à 0. cette somme là va prendre tous les éléments de l'array en entrée.
+	arr.each do |x| # On va parcourir chaque élément de l'array en Input, en utilisant array.each {|x| .. } et on l'ajoute à la somme qui sera en résultat
 		res += x
 	end
 	return res
@@ -18,7 +18,7 @@ end
 
 def multiply (arr)
 	multi = 0
-	arr.each do |x|
+	arr.each do |x| #On fait la même chose pour la somme, sauf que pour cette fois aux mieux d'additionner les éléments sur somme, on va au contraire multiplier les éléments 
 		multi *= x
 	end
 	return multi 
